45b2f24775ecd6c6282a0080e02ce4fceba330af
951237 <951237@gmail.com>
2022-08-19 01:08:52 +0900

    Update : 데이터 현황 추가

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ df_selection = df.query(
 
 # --- 메인 페이지 ---
 st.title(":bar_chart: 안산부곡초 재적인원 현황판")
-st.subtitle("22.8.15 현재")
+st.header("22.8.15 현재")
 
 # 화면 타이틀
 st.markdown("##")		# 마크다운 문법 가능
