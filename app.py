6318ceb57588db8fc5c51b6e6ba8c6ba8928dc46
951237 <951237@gmail.com>
2022-09-18 00:10:38 +0900

    Update : 엑셀파일 기본값을 마지막 값으로 설정하기

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def get_excelfile(p_file):
 	return df
 
 lst_xlsx = read_xlsx_files()
+default_ix = lst_xlsx[-1]
 
 # st.dataframe(df)
 
@@ -56,7 +57,7 @@ st.sidebar.header("Please Filter Here:")	# 사이드바 헤더(제목)
 file_xlsxs = st.sidebar.selectbox(
 	"Select data file:",
 	lst_xlsx,
-	index=lst_xlsx[-1]
+	index=default_ix
 )
 
 # 데이터프레임 만들기
